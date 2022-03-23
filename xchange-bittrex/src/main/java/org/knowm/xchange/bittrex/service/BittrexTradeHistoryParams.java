55c40a9b04143c57fd64ead851e4b3f05982a95f
martinkyov <martin.kyovsky@generalbytes.com>
2022-03-23 11:19:31 +0100

    ETD-370:change paging from timeStamps to Ids.

--- xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/service/BittrexTradeHistoryParams.java
+++ xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/service/BittrexTradeHistoryParams.java
@@ -1,16 +1,15 @@
 package org.knowm.xchange.bittrex.service;
 
-import java.util.Date;
 import lombok.Data;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.service.trade.params.TradeHistoryParamCurrencyPair;
 import org.knowm.xchange.service.trade.params.TradeHistoryParams;
-import org.knowm.xchange.service.trade.params.TradeHistoryParamsTimeSpan;
+import org.knowm.xchange.service.trade.params.TradeHistoryParamsIdSpan;
 
 @Data
 public class BittrexTradeHistoryParams
-    implements TradeHistoryParams, TradeHistoryParamCurrencyPair, TradeHistoryParamsTimeSpan {
-  private Date startTime;
-  private Date endTime;
+    implements TradeHistoryParams, TradeHistoryParamCurrencyPair, TradeHistoryParamsIdSpan {
+  private String startId;
+  private String endId;
   private CurrencyPair currencyPair;
 }

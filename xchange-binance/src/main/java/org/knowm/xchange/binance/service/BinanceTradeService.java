0bf9011235992d48c43e8d9d9cb47fdafae09e49
martinkyov <martin.kyovsky@generalbytes.com>
2022-09-16 11:07:38 +0200

    ETD-424:connectors-convert-transaction

--- xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceTradeService.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceTradeService.java
@@ -245,6 +245,7 @@ public class BinanceTradeService extends BinanceTradeServiceRaw implements Trade
   public UserTrades getConvertHistory(TradeHistoryParams params) throws IOException {
     Date today = new Date();
     long endTime = today.getTime();
+
     long i1 = (1000L * 60 * 60 * 24) * 29;
     long startTime = endTime - i1;// before 29 days;
     int limit = 1000;

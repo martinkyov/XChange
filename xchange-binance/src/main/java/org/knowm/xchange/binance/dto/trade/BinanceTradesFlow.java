ef44c8b560c1b73e618b51e984931170b6926c70
martinkyov <martin.kyovsky@generalbytes.com>
2022-09-19 10:09:24 +0200

    ETD-424:connectors-convert-transaction

--- xchange-binance/src/main/java/org/knowm/xchange/binance/dto/trade/BinanceTradesFlow.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/dto/trade/BinanceTradesFlow.java
@@ -4,14 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class BinanceTradesFlow {
-    @JsonProperty("list") String converts;
-    @JsonProperty("startTime") String startTime;
-    @JsonProperty("endTime") String endTime;
-    @JsonProperty("limit") String limit;
-    @JsonProperty("moreData") String moreData;
+    @JsonProperty("list") Object converts;
 }

b03119511eecc49a342f6a5f34febbcb3fefe2be
martinkyov <martin.kyovsky@generalbytes.com>
2022-09-19 09:53:00 +0200

    ETD-424:connectors-convert-transaction

--- xchange-binance/src/main/java/org/knowm/xchange/binance/dto/trade/BinanceTradesFlow.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/dto/trade/BinanceTradesFlow.java
@@ -1,47 +1,17 @@
 package org.knowm.xchange.binance.dto.trade;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
 public class BinanceTradesFlow {
-    String converts;
-    String startTime;
-    String endTime;
-    String limit;
-    String moreData;
-
-    public BinanceTradesFlow(
-            @JsonProperty("list") String converts,
-            @JsonProperty("startTime") String startTime,
-            @JsonProperty("endTime") String endTime,
-            @JsonProperty("limit") String limit,
-            @JsonProperty("moreData") String moreData
-    ) {
-        this.converts = converts;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.limit = limit;
-        this.moreData = moreData;
-    }
-
-    public String getConverts() {
-        return converts;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public String getLimit() {
-        return limit;
-    }
-
-    public String isMoreData() {
-        return moreData;
-    }
+    @JsonProperty("list") String converts;
+    @JsonProperty("startTime") String startTime;
+    @JsonProperty("endTime") String endTime;
+    @JsonProperty("limit") String limit;
+    @JsonProperty("moreData") String moreData;
 }
